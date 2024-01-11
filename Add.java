87ed222fea2a0b44d8d654f265dc1e961b7a93f9
nandhakkmma <nithyanandakomma123@gmail.com>
2024-01-11 10:02:26 +0530

    version

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add{
+  public static void main (String[] args) {
+   int num1=7;
+   int num2=9;
+   int num3=123;
+    int sum=num1+num2+num3;
+    System.out.print("the sum is ");
+   System.out.println(sum);
+  }
