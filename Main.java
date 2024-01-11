0f475c4fab4c54b7a1fe7a1dc463bc86dc4e9e35
nandhakkmma <nithyanandakomma123@gmail.com>
2024-01-11 10:03:25 +0530

    version

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-  public static void main (String[] args) {
-   int num1=7;
-   int num2=9;
-   int num3=123;
-    int sum=num1+num2+num3;
-    System.out.print("the sum is ");
-   System.out.println(sum);
-  }
